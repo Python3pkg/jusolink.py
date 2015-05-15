e53c33d02fa5db92f8f0c2e6ed3e85e0a254e07e
JohnJeong123 <frenchofkiss@gmail.com>
2015-05-15 18:01:28 +0900

    fixed PerPage

--- jusolink/jusolink.py
+++ jusolink/jusolink.py
@@ -129,8 +129,6 @@ class Jusolink(__with_metaclass(Singleton,object)):
             if PerPage != None :
                 if PerPage < 0 :
                     PerPage = 20 
-                if PerPage > 100 :
-                    PerPage = 100
 
             url = "/Search?Searches="
  
